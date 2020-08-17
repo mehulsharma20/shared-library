dd6f341e48b18a9d4affc71cf955dfd0f9700155
mehulsharma20 <sharma.mehul20@gmail.com>
2020-08-17 23:59:31 +0530

    updates

--- vars/sharedlibrary.groovy
+++ vars/sharedlibrary.groovy
@@ -1,16 +1,7 @@
 def buildNdeploy(){
         dir('/home/mehul/Desktop/shared-lib/vars') {
         stage("build frontend project"){
-            try{
-             //  sh "./frontend.sh" 
-             echo "running build"
-            }
-            catch(Exception e){
-                echo "build failed"
-//                stage("Revert back."){
-                 }
-                throw e 
-              }
+            sh "./frontend.sh"
             }
                }
-               //}
+            
